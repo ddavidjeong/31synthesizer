553fd70626896ea501f918c42dd34320a9ff2c67
David <ddavidjeong@gmail.com>
2022-03-16 18:59:16 -0400

    update 1.1

--- test/main.ml
+++ test/main.ml
@@ -2,12 +2,6 @@ open OUnit2
 open Synth
 open Mm_audio
 
-
-let () =  
-let buf = Audio.create 2 1000 in
-new Audio.Generator.of_mono
-    (new Audio.Mono.Generator.sine sample_rate (440.0 *. freq))
-in 
-for _ = 0 to 10 do 
-  sine#fill buf 0 1024;
-done;
+let sine_sound =
+  new Audio.Generator.of_mono
+    (new Audio.Mono.Generator.sine 44000 440.0)
