34b77b2ec9c31d86c91fd92348e0adc19a2647dd
Ricardo Cruz <rlc357@cornell.edu>
2022-03-30 22:30:56 -0400

    Started work on filters

--- test/main.ml
+++ test/main.ml
@@ -18,31 +18,56 @@ let get_wave = function
 let fst (x, _, _) = x
 let snd (_, y, _) = y
 let thd (_, _, z) = z
+let play_using_generator input = 
+  let total_duration = snd input in
+  let channels = 2 in
+  let sample_rate = 44100 in
+  let freq = fst input in
+  let ao = new Mm_ao.writer channels sample_rate in
+  let wav = new Audio.IO.Writer.to_wav_file channels sample_rate "out.wav" in
+  let blen = 1024 in
+  let buf = Audio.create channels blen in
+  let sine =
+    new Audio.Generator.of_mono (new Audio.Mono.Generator.sine sample_rate freq)
+  in
+  let loop_indices = (sample_rate / blen * total_duration) - 1 in
+  let prev_buf = ref (Audio.create 2 0) in
+  for x = 0 to loop_indices do
+    sine#fill buf;
+    Audio.to_array buf;
+    wav#write buf;
+    ao#write buf;
+    prev_buf := buf
+  done;
+  wav#close;
+  ao#close
 
-let play_sound input =
+let play_using_synth input = 
   let total_duration = snd input in
   let frequency = fst input in
   let channels = 4 in
-  let sample_rate = 2000 in
+  let sample_rate = 44100 in
   (* let ao = new Mm_ao.writer channels sample_rate in *)
   (* let wav = new Audio.IO.Writer.to_wav_file channels sample_rate
      "../output/out.wav" in *)
   (* let wave = Synth.sound *)
-  let blen = 1024 in
+  let blen = sample_rate * total_duration in
   let wave = get_wave (thd input) in
-  let sound =
-    Synth__Sound.new_wave wave frequency sample_rate channels blen
-  in
-  for _ = 0 to sample_rate / blen * total_duration do
+  let sound = Synth__Sound.new_wave wave frequency sample_rate channels blen in
+  Synth__Sound.start sound;
+  (*for _ = 0 to (sample_rate / blen) * total_duration do
     Synth__Sound.start sound
-  done;
+  done;*)
   Synth__Sound.release sound
-
-(* let buf = Audio.create channels blen in let sine = new
-   Audio.Generator.of_mono (get_wave (thd input) sample_rate frequency)
-   in for _ = 0 to (sample_rate / blen * total_duration) - 1 do
+(* let buf = Audio.create channels blen in 
+   let sine = new Audio.Generator.of_mono (get_wave (thd input) sample_rate frequency) in 
+   for _ = 0 to (sample_rate / blen * total_duration) - 1 do
    sine#fill buf; wav#write buf; ao#write buf done; wav#close;
    ao#close *)
+
+let play_sound input =
+  (*play_using_synth input*)
+  play_using_generator input
 
 let rec record_sound io input =
   let total_duration = snd input in
@@ -81,7 +106,7 @@ let play =
       \       To record, please enter \"record\". Then, input the \
        required data.\n\
       \       To stop recording, enter \"stop\"\n\
-      \       To exit the interface, enter \"quit\"";
+      \       To exit the interface, enter \"quit\"\n> ";
     match read_line () with
     | x when String.trim x = "quit" -> Stdlib.exit 0
     | "record" ->
