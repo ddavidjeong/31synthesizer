4426fdda95d976aaebbfec663b27e805a60435b8
Sean <yh797@cornell.edu>
2022-05-01 15:04:49 -0400

    implemented GUI using Sdl library

--- bin/main2.ml
+++ bin/main2.ml
@@ -1,71 +1,251 @@
 open Graphics
 open Printf
 open Synth
+open Sdlevent
+open Sdl
 
-(* Displays mouse position and keys pressed in the graphics window, and
-   exits if q is pressed. *)
+let sample_rate = 1000
+let channels = 4
+let buf_len = 1024
 
-let print_at x y message =
-  moveto x y;
-  draw_string message
+let init_sound_A =
+  ref (Sound.new_wave Sound.Sine 0. sample_rate channels buf_len)
 
-let rec loop () =
-  print_at 45 525 "Note: C";
-  print_at 165 525 "Note: D";
-  print_at 285 525 "Note: E";
-  print_at 405 525 "Note: F";
-  print_at 525 525 "Note: G";
-  draw_rect 20 480 100 100;
-  draw_rect 140 480 100 100;
-  draw_rect 260 480 100 100;
-  draw_rect 380 480 100 100;
-  draw_rect 500 480 100 100;
+let io = ref (IO.init_io channels sample_rate "plswork")
+let record_button = Gui.create_button 100 200
+let sine_button = Gui.create_button 175 200
+let () = Gui.set_clicked sine_button true
+let saw_button = Gui.create_button 250 200
+let square_button = Gui.create_button 325 200
+let triangle_button = Gui.create_button 400 200
+let which_wave = ref "sine"
 
-  let e = wait_next_event [ Key_pressed ] in
-  let key_description =
-    if e.keypressed then sprintf "Key %c was pressed" e.key else ""
+let buttons =
+  [
+    record_button;
+    sine_button;
+    saw_button;
+    square_button;
+    triangle_button;
+  ]
+
+let wave_buttons =
+  [ sine_button; saw_button; square_button; triangle_button ]
+
+(* creates the initial screen *)
+let init_render renderer butt_lst =
+  let print_x_padding x y padding lst =
+    List.iteri
+      (fun index charac ->
+        Gui.letters renderer charac (x + (index * padding)) y)
+      lst
   in
 
-  let mouse_description =
-    sprintf "Mouse position: %d,%d" e.mouse_x e.mouse_y
+  Sdlrender.set_draw_color renderer (255, 255, 255) 255;
+  Sdlrender.clear renderer;
+
+  (* DONT TOUCH THIS *)
+  Sdlrender.set_draw_color renderer (0, 0, 0) 255;
+  Sdlrender.draw_rects renderer
+    [|
+      (* white keys *)
+      { x = 100; y = 100; w = 50; h = 50 };
+      { x = 164; y = 100; w = 50; h = 50 };
+      { x = 228; y = 100; w = 50; h = 50 };
+      { x = 292; y = 100; w = 50; h = 50 };
+      { x = 354; y = 100; w = 50; h = 50 };
+      { x = 418; y = 100; w = 50; h = 50 };
+      { x = 482; y = 100; w = 50; h = 50 };
+      { x = 546; y = 100; w = 50; h = 50 };
+      (* black keys *)
+      { x = 132; y = 40; w = 50; h = 50 };
+      { x = 196; y = 40; w = 50; h = 50 };
+      { x = 323; y = 40; w = 50; h = 50 };
+      { x = 387; y = 40; w = 50; h = 50 };
+      { x = 451; y = 40; w = 50; h = 50 };
+      (* waveform keys *)
+      { x = 100; y = 200; w = 50; h = 50 };
+      { x = 175; y = 200; w = 50; h = 50 };
+      { x = 250; y = 200; w = 50; h = 50 };
+      { x = 325; y = 200; w = 50; h = 50 };
+      { x = 400; y = 200; w = 50; h = 50 };
+    |];
+
+  (* print key on keys*)
+  (* (Gui.letters renderer).o 120 120; *)
+
+  (* "Notes: C D E F G A B C" *)
+  print_x_padding 50 20 8 [ 'n'; 'o'; 't'; 'e'; 's'; ':' ];
+  print_x_padding 147 20 64 [ 'c'; 'd'; ' '; 'f'; 'g'; 'a' ];
+  print_x_padding 155 20 64 [ '#'; '#'; ' '; '#'; '#'; '#' ];
+  print_x_padding 152 60 64 [ 'w'; 'e'; ' '; 't'; 'y'; 'u' ];
+  print_x_padding 120 118 64 [ 'a'; 's'; 'd'; 'f'; 'g'; 'h'; 'j'; 'k' ];
+  print_x_padding 50 160 8 [ 'n'; 'o'; 't'; 'e'; 's'; ':' ];
+  print_x_padding 120 160 64 [ 'c'; 'd'; 'e'; 'f'; 'g'; 'a'; 'b'; 'c' ];
+
+  (* function buttons *)
+  print_x_padding 113 220 9 [ 'r'; 'e'; 'c' ];
+  print_x_padding 190 260 8 [ 's'; 'i'; 'n' ];
+  print_x_padding 260 260 8 [ 's'; 'a'; 'w' ];
+  print_x_padding 340 260 9 [ 's'; 'q'; 'r' ];
+  print_x_padding 410 260 8 [ 't'; 'r'; 'i' ];
+
+  let rec init_buttons butt_lst =
+    match butt_lst with
+    | [] -> ()
+    | button :: rest ->
+        if Gui.get_clicked button then (
+          Sdlrender.fill_rect renderer
+            {
+              x = Gui.get_x button;
+              y = Gui.get_y button;
+              w = 50;
+              h = 50;
+            };
+          init_buttons rest)
+        else init_buttons rest
   in
-  print_at 300 150 mouse_description;
-  if e.keypressed then print_at 100 100 key_description;
+  init_buttons butt_lst
 
-  clear_graph ();
-  print_at 45 525 "Note: C";
-  print_at 165 525 "Note: D";
-  print_at 285 525 "Note: E";
-  print_at 405 525 "Note: F";
-  print_at 525 525 "Note: G";
-  draw_rect 20 480 100 100;
-  draw_rect 140 480 100 100;
-  draw_rect 260 480 100 100;
-  draw_rect 380 480 100 100;
-  draw_rect 500 480 100 100;
+(* fills in key color when key is pressed *)
+let press_note_key renderer (e : keyboard_event) =
+  let keycode = e.keycode in
+  let fill_at x y =
+    Sdlrender.set_draw_color renderer (0, 0, 0) 255;
+    Sdlrender.fill_rect renderer { x; y; w = 50; h = 50 };
+    Sdlrender.render_present renderer
+  in
+  match keycode with
+  | A -> fill_at 100 100
+  | S -> fill_at 164 100
+  | D -> fill_at 228 100
+  | F -> fill_at 292 100
+  | G -> fill_at 354 100
+  | H -> fill_at 418 100
+  | J -> fill_at 482 100
+  | K -> fill_at 546 100
+  | W -> fill_at 132 40
+  | E -> fill_at 196 40
+  | T -> fill_at 323 40
+  | Y -> fill_at 387 40
+  | U -> fill_at 451 40
+  | _ -> ()
 
-  set_color black;
-  if e.key == 'q' then fill_rect 20 480 100 100;
-  if e.key == 'w' then fill_rect 140 480 100 100;
-  if e.key == 'e' then fill_rect 260 480 100 100;
-  if e.key == 'r' then fill_rect 380 480 100 100;
-  if e.key == 't' then fill_rect 500 480 100 100;
-  Synth__Gui.key_pressed e;
-  let test = sprintf "test: %B" e.button in
-  print_at 200 150 test;
+(* fills in button color when button is pressed and clears it when
+   pressed again*)
+let press_button x y renderer button =
+  if Gui.get_clicked button = true then (
+    (* button is clicked; change to unclicked*)
+    Sdlrender.set_draw_color renderer (255, 255, 255) 255;
+    Sdlrender.fill_rect renderer { x; y; w = 50; h = 50 };
+    Sdlrender.set_draw_color renderer (0, 0, 0) 255;
+    Sdlrender.draw_rect renderer { x; y; w = 50; h = 50 };
+    Sdlrender.render_present renderer;
+    Gui.set_clicked button false)
+  else (
+    (* button is not clicked; change to clicked *)
+    Sdlrender.set_draw_color renderer (0, 0, 0) 255;
+    Sdlrender.fill_rect renderer { x; y; w = 50; h = 50 };
+    Sdlrender.render_present renderer;
+    Gui.set_clicked button true)
 
-  draw_rect 20 20 100 100;
+let rec blank_waveform_buttons renderer butt_lst =
+  match butt_lst with
+  | [] -> ()
+  | button :: rest ->
+      Gui.set_clicked button false;
+      Sdlrender.set_draw_color renderer (255, 255, 255) 255;
+      Sdlrender.fill_rect renderer
+        { x = Gui.get_x button; y = Gui.get_y button; w = 50; h = 50 };
+      Sdlrender.set_draw_color renderer (0, 0, 0) 255;
+      Sdlrender.draw_rect renderer
+        { x = Gui.get_x button; y = Gui.get_y button; w = 50; h = 50 };
+      blank_waveform_buttons renderer rest
 
-  set_color white;
-  if e.key == 'q' then fill_rect 20 480 100 100;
-  if e.key == 'w' then fill_rect 140 480 100 100;
-  if e.key == 'e' then fill_rect 260 480 100 100;
-  if e.key == 'r' then fill_rect 380 480 100 100;
-  if e.key == 't' then fill_rect 500 480 100 100;
+let press_only_one_button x y renderer butt_lst =
+  blank_waveform_buttons renderer butt_lst;
+  Sdlrender.set_draw_color renderer (0, 0, 0) 255;
+  Sdlrender.fill_rect renderer { x; y; w = 50; h = 50 };
+  Sdlrender.render_present renderer
 
-  if e.key <> 'p' then loop () else ()
+(* helper function *)
+let range butx buty x y w =
+  butx < x + w && butx > x && buty < y + w && buty > y
+
+(* used to specify different functionalities for different buttons*)
+let mouseclick_button e renderer =
+  let x = e.mb_x in
+  let y = e.mb_y in
+  (* record button code *)
+  if range x y 100 200 50 then (
+    press_button 100 200 renderer record_button;
+    if Gui.get_clicked record_button = false then IO.stop_recording !io
+    else io := IO.init_io channels sample_rate "plswork"
+      (* wave buttons*))
+  else if range x y 175 200 50 then (
+    press_only_one_button 175 200 renderer wave_buttons;
+    which_wave := "sine";
+    Gui.set_clicked sine_button true)
+  else if range x y 250 200 50 then (
+    press_only_one_button 250 200 renderer wave_buttons;
+    which_wave := "saw";
+    Gui.set_clicked saw_button true)
+  else if range x y 325 200 50 then (
+    press_only_one_button 325 200 renderer wave_buttons;
+    which_wave := "square";
+    Gui.set_clicked square_button true)
+  else if range x y 400 200 50 then (
+    press_only_one_button 400 200 renderer wave_buttons;
+    which_wave := "triangle";
+    Gui.set_clicked triangle_button true)
+  else ()
+
+let proc_events renderer = function
+  (* | KeyDown { scancode = Sdlscancode.ESCAPE } -> print_endline
+     "Goodbye"; exit 0 *)
+  | KeyDown { keycode = Sdlkeycode.Escape } ->
+      print_endline "Bye!";
+      exit 0
+  | KeyDown e ->
+      press_note_key renderer e;
+      let sound = Synth.Gui.key_pressed e !which_wave in
+      Sound.start sound;
+      if Gui.get_clicked record_button = true then
+        for _ = 0 to (sample_rate / buf_len) - 1 do
+          IO.record sound !io
+        done;
+      Sound.release sound
+  (* init_sound_A := Synth.Gui.key_pressed e; *)
+  (* Sound.start !init_sound_A *)
+  (* | KeyUp { keycode = Sdlkeycode.A } -> Sound.release
+     !init_sound_A *)
+  (* Synth.Gui.key_pressed e *)
+  | KeyUp e ->
+      init_render renderer buttons;
+      Sdlrender.render_present renderer (* Synth.Gui.key_pressed e *)
+  | Mouse_Button_Down e -> mouseclick_button e renderer
+  | _ -> Printf.printf ""
 
 let () =
-  Graphics.open_graph " 800x600";
-  loop ();
-  close_graph ()
+  Random.self_init ();
+  let width, height = (680, 300) in
+  Sdl.init [ `VIDEO ];
+  let window, renderer =
+    Sdlrender.create_window_and_renderer ~width ~height ~flags:[]
+  in
+  init_render renderer buttons;
+  Sdlrender.render_present renderer;
+  let rec aux () =
+    init_render renderer buttons;
+    let xy, buttons = Sdlmouse.get_state () in
+    Sdlrender.draw_point renderer xy;
+
+    Sdltimer.delay 10;
+    match Sdlevent.poll_event () with
+    | Some (Sdlevent.Quit _) -> Sdl.quit ()
+    | Some ev ->
+        proc_events renderer ev;
+        aux ()
+    | _ -> aux ()
+  in
+  aux ()
--- src/gui.ml
+++ src/gui.ml
@@ -1,89 +1,376 @@
-open Graphics
+open Sdlevent
 open Printf
+open Sdl
 (* open Sound *)
+
+type button = {
+  x : int;
+  y : int;
+  mutable clicked : bool;
+}
+
+let letters renderer char =
+  match char with
+  | 'a' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 10 };
+            { x; y; w = 5; h = 2 };
+            { x; y = y + 3; w = 5; h = 2 };
+            { x = x + 5; y; w = 2; h = 10 };
+          |]
+  | 'b' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 10 };
+            { x = x + 5; y = y + 5; w = 2; h = 5 };
+            { x = x + 3; y; w = 2; h = 5 };
+            { x; y; w = 3; h = 2 };
+            { x; y = y + 4; w = 7; h = 2 };
+            { x; y = y + 8; w = 5; h = 2 };
+          |]
+  | 'c' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 10 };
+            { x; y; w = 7; h = 2 };
+            { x; y = y + 8; w = 7; h = 2 };
+          |]
+  | 'd' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 10 };
+            { x; y; w = 3; h = 2 };
+            { x; y = y + 8; w = 3; h = 2 };
+            { x = x + 5; y = y + 3; w = 1; h = 3 };
+          |];
+        Sdlrender.draw_lines renderer
+          [|
+            (x + 3, y);
+            (x + 5, y + 2);
+            (x + 3, y + 1);
+            (x + 5, y + 3);
+            (x + 3, y + 9);
+            (x + 5, y + 7);
+            (x + 3, y + 8);
+            (x + 5, y + 6);
+          |]
+  | 'e' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 10 };
+            { x; y; w = 7; h = 2 };
+            { x; y = y + 8; w = 7; h = 2 };
+            { x; y = y + 4; w = 5; h = 2 };
+          |]
+  | 'f' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 10 };
+            { x; y; w = 7; h = 2 };
+            { x; y = y + 4; w = 5; h = 2 };
+          |]
+  | 'g' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 10 };
+            { x; y; w = 7; h = 2 };
+            { x; y = y + 8; w = 7; h = 2 };
+            { x = x + 5; y = y + 5; w = 2; h = 5 };
+            { x = x + 3; y = y + 4; w = 4; h = 2 };
+          |]
+  | 'h' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 10 };
+            { x = x + 5; y; w = 2; h = 10 };
+            { x; y = y + 4; w = 5; h = 2 };
+          |]
+  | 'i' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x = x + 2; y; w = 2; h = 10 };
+            { x; y; w = 6; h = 2 };
+            { x; y = y + 8; w = 6; h = 2 };
+          |]
+  | 'j' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x = x + 3; y; w = 2; h = 10 };
+            { x; y = y + 8; w = 3; h = 2 };
+            { x; y; w = 7; h = 2 };
+          |]
+  | 'k' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer [| { x; y; w = 2; h = 10 } |];
+        Sdlrender.draw_lines renderer
+          [|
+            (x, y + 5);
+            (x + 7, y);
+            (x + 7, y + 1);
+            (x, y + 6);
+            (x + 7, y + 10);
+            (x + 7, y + 9);
+            (x, y + 5);
+          |]
+  | 'l' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [| { x; y; w = 2; h = 10 }; { x; y = y + 8; w = 7; h = 2 } |]
+  | 'm' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [| { x; y; w = 2; h = 10 }; { x = x + 7; y; w = 2; h = 10 } |];
+        Sdlrender.draw_lines renderer
+          [|
+            (x + 4, y + 4);
+            (x + 7, y + 1);
+            (x + 7, y);
+            (x + 4, y + 3);
+            (x, y);
+            (x, y + 1);
+            (x + 4, y + 3);
+          |]
+  | 'n' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [| { x; y; w = 2; h = 10 }; { x = x + 5; y; w = 2; h = 10 } |];
+        Sdlrender.draw_lines renderer
+          [| (x, y); (x + 6, y + 8); (x + 6, y + 9); (x, y + 1) |]
+  | 'o' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 10 };
+            { x = x + 5; y; w = 2; h = 10 };
+            { x; y; w = 7; h = 2 };
+            { x; y = y + 8; w = 7; h = 2 };
+          |]
+  | 'p' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 10 };
+            { x = x + 5; y; w = 2; h = 5 };
+            { x; y; w = 7; h = 2 };
+            { x; y = y + 5; w = 7; h = 2 };
+          |]
+  | 'q' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 10 };
+            { x = x + 5; y; w = 2; h = 10 };
+            { x; y; w = 7; h = 2 };
+            { x; y = y + 8; w = 9; h = 2 };
+          |]
+  | 'r' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 10 };
+            { x = x + 5; y; w = 2; h = 5 };
+            { x; y; w = 7; h = 2 };
+            { x; y = y + 4; w = 7; h = 2 };
+          |];
+        Sdlrender.draw_lines renderer
+          [|
+            (x + 2, y + 4);
+            (x + 7, y + 9);
+            (x + 6, y + 9);
+            (x + 1, y + 4);
+          |]
+  | 's' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 5; h = 2 };
+            { x; y = y + 4; w = 5; h = 2 };
+            { x; y = y + 8; w = 5; h = 2 };
+            { x; y; w = 2; h = 5 };
+            { x = x + 3; y = y + 4; w = 2; h = 5 };
+          |]
+  | 't' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [| { x = x + 2; y; w = 2; h = 10 }; { x; y; w = 6; h = 2 } |]
+  | 'u' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 10 };
+            { x = x + 5; y; w = 2; h = 10 };
+            { x; y = y + 8; w = 7; h = 2 };
+          |]
+  | 'v' ->
+      fun x y ->
+        Sdlrender.draw_lines renderer
+          [|
+            (x, y);
+            (x + 2, y + 9);
+            (x + 3, y + 9);
+            (x + 5, y);
+            (x + 4, y);
+            (x + 3, y + 8);
+            (x + 2, y + 8);
+            (x + 1, y);
+          |]
+  | 'w' ->
+      fun x y ->
+        Sdlrender.draw_lines renderer
+          [|
+            (x, y);
+            (x + 2, y + 9);
+            (x + 3, y + 9);
+            (x + 5, y);
+            (x + 4, y);
+            (x + 3, y + 8);
+            (x + 2, y + 8);
+            (x + 1, y);
+            (x + 5, y);
+            (x + 7, y + 9);
+            (x + 8, y + 9);
+            (x + 10, y);
+            (x + 9, y);
+            (x + 8, y + 8);
+            (x + 7, y + 8);
+            (x + 6, y);
+          |]
+  | 'y' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y; w = 2; h = 4 };
+            { x = x + 5; y; w = 2; h = 4 };
+            { x; y = y + 3; w = 7; h = 2 };
+            { x = x + 3; y = y + 3; w = 2; h = 7 };
+          |]
+  | ':' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x; y = y + 2; w = 2; h = 2 };
+            { x; y = y + 6; w = 2; h = 2 };
+          |]
+  | '#' ->
+      fun x y ->
+        Sdlrender.draw_rects renderer
+          [|
+            { x = x + 1; y; w = 2; h = 10 };
+            { x = x + 4; y; w = 2; h = 10 };
+            { x; y = y + 2; w = 7; h = 2 };
+            { x; y = y + 5; w = 7; h = 2 };
+          |]
+  | _ -> fun x y -> ()
 
 let sample_rate = 1000
 let channels = 4
 let buf_len = 1024
-let sounds = ref []
+let create_button x y = { x; y; clicked = false }
+let get_clicked button = button.clicked
+let get_x button = button.x
+let get_y button = button.y
+let set_clicked button v = button.clicked <- v
+(* let key_pressed (e : keyboard_event) (sound : Sound.synth) = (* let
+   dur = 1 in *) (* if e.keypressed then *) match e.keycode with | A ->
+   (* let freq = 261. in let wave = Sound.new_wave Sound.Sine freq
+   sample_rate channels buf_len in *) (* Sound.start wave; *)
+   Sound.set_freq sound 261.; Printf.printf "works \n" | _ ->
+   Printf.printf "" *)
 
-let key_pressed e =
+let match_wave wave freq =
+  match wave with
+  | "sine" ->
+      Sound.new_wave Sound.Sine freq sample_rate channels buf_len
+  | "saw" -> Sound.new_wave Sound.Saw freq sample_rate channels buf_len
+  | "square" ->
+      Sound.new_wave Sound.Square freq sample_rate channels buf_len
+  | "triangle" ->
+      Sound.new_wave Sound.Triangle freq sample_rate channels buf_len
+  | _ -> Sound.new_wave Sound.Sine freq sample_rate channels buf_len
+
+let key_pressed (e : keyboard_event) (wave : string) =
   (* let dur = 1 in *)
   (* if e.keypressed then *)
-  match e.key with
-  | 'a' ->
+  (* let sound = *)
+  match e.keycode with
+  | A ->
+      (* C *)
       let freq = 261. in
-      let wave =
-        Sound.new_wave Sound.Sine freq sample_rate channels buf_len
-      in
-      Sound.start wave;
-      if e.keypressed = false then Sound.release wave
-  | 's' ->
+      let wave = match_wave wave freq in
+      (* Sound.start wave; *)
+      wave
+  | W ->
+      (* C# *)
+      let freq = 277. in
+      let wave = match_wave wave freq in
+      wave
+  | S ->
+      (* D *)
       let freq = 293. in
-      let wave =
-        Sound.new_wave Sound.Sine freq sample_rate channels buf_len
-      in
-      Sound.start wave;
-      if e.keypressed = false then Sound.release wave
-  | 'd' ->
+      let wave = match_wave wave freq in
+      (* Sound.start wave; *) wave
+  | E ->
+      (* D# *)
+      let freq = 311. in
+      let wave = match_wave wave freq in
+      wave
+  | D ->
+      (* E *)
       let freq = 329. in
-      let wave =
-        Sound.new_wave Sound.Sine freq sample_rate channels buf_len
-      in
-      Sound.start wave;
-      if e.keypressed = false then Sound.release wave
-  | 'f' ->
+      let wave = match_wave wave freq in
+      (* Sound.start wave; *) wave
+  | F ->
+      (* F *)
       let freq = 349. in
-      let wave =
-        Sound.new_wave Sound.Sine freq sample_rate channels buf_len
-      in
-      Sound.start wave;
-      if e.keypressed = false then Sound.release wave
-  | 'g' ->
+      let wave = match_wave wave freq in
+      (* Sound.start wave; *) wave
+  | T ->
+      (* F# *)
+      let freq = 370. in
+      let wave = match_wave wave freq in
+      (* Sound.start wave; *) wave
+  | G ->
+      (* G *)
       let freq = 392. in
-      let wave =
-        Sound.new_wave Sound.Sine freq sample_rate channels buf_len
-      in
-      Sound.start wave;
-      if e.keypressed = false then Sound.release wave
-  | 'h' ->
+      let wave = match_wave wave freq in
+      (* Sound.start wave; *) wave
+  | Y ->
+      (* G# *)
+      let freq = 415. in
+      let wave = match_wave wave freq in
+      (* Sound.start wave; *) wave
+  | H ->
+      (* A *)
       let freq = 440. in
-      let wave =
-        Sound.new_wave Sound.Sine freq sample_rate channels buf_len
-      in
-      Sound.start wave;
-      if e.keypressed = false then Sound.release wave
-  | 'j' ->
+      let wave = match_wave wave freq in
+      (* Sound.start wave; *) wave
+  | U ->
+      (* A# *)
+      let freq = 466. in
+      let wave = match_wave wave freq in
+      (* Sound.start wave; *) wave
+  | J ->
+      (* B *)
       let freq = 494. in
-      let wave =
-        Sound.new_wave Sound.Sine freq sample_rate channels buf_len
-      in
-      Sound.start wave;
-      if e.keypressed = false then Sound.release wave
-  | 'k' ->
+      let wave = match_wave wave freq in
+      (* Sound.start wave; *) wave
+  | K ->
+      (* C *)
       let freq = 523. in
-      let wave =
-        Sound.new_wave Sound.Sine freq sample_rate channels buf_len
-      in
-      Sound.start wave;
-      if e.keypressed = false then Sound.release wave
-  | _ -> begin
-      match !sounds with
-      | (ch, wv) :: _ ->
-          print_string "um";
-          Sound.release wv
-      | _ -> Printf.printf ""
-    end
-(* else *)
-
-let make_mouse_button x y w h e =
-  set_color black;
-  draw_rect x y w h;
-  if
-    e.button && x < e.mouse_x
-    && e.mouse_x < x + w
-    && y < e.mouse_y
-    && e.mouse_y < y + h
-  then (
-    set_color black;
-    fill_rect x y w h)
+      let wave = match_wave wave freq in
+      (* Sound.start wave; *) wave
+  | _ -> Sound.new_wave Sound.Sine 0. sample_rate channels buf_len
+(* in sound *)
+(* if e.ke_state = Pressed then Sound.start sound *)
+(* else Sound.release sound *)
