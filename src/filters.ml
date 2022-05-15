3298bd92d50dfd71b48cd6a3b78664dee85c0f7b
Sean <yh797@cornell.edu>
2022-05-15 19:40:30 -0400

    Pushing filters.ml

--- src/filters.ml
+++ src/filters.ml
@@ -23,41 +23,43 @@ let print_float_array a =
   print_string " |]\n"
 
 (* Applies function f to each channel of an audio array. *)
-<<<<<<< Updated upstream
-let all_channels a f = 
-  for c = 0 to Array.length a - 1 do f a.(c) done
-
-let all_channels_2 a1 a2 f = 
-  for c = 0 to Array.length a1 - 1 do f a1.(c) a2.(c) done
-
-(*  Mutates an array in-place such that each element is the average of itself
-    and the next n elements. *)
-let blur n a = 
-  for i = 0 to Array.length a - 1 - n do
-    a.(i) <- (a.(i) +. a.(i + n)) /. float_of_int n
+let all_channels a f =
+  for c = 0 to Array.length a - 1 do
+    f a.(c)
   done
 
-(*  Smoothing lowpass filter example adapted to OCaml from:
-    http://phrogz.net/js/framerate-independent-low-pass-filter.html*)
-let smooth (smoothing : float) (values : float array) = 
-  for i = 0 to Array.length values - 1 do
-    let current_value = values.(i) in 
-    let new_value = current_value *. (smoothing ** float_of_int i) in
-    values.(i) <- new_value
+let all_channels_2 a1 a2 f =
+  for c = 0 to Array.length a1 - 1 do
+    f a1.(c) a2.(c)
   done
 
-let envelope (factor : float) (index : int) last_index buf = 
-  let indexf = float_of_int index in 
-  let last_indexf = float_of_int last_index in 
-  let inc_coeff = mod_float indexf (last_indexf) /. factor |> Float.abs in 
-  let dec_coeff = mod_float (last_indexf -. indexf) (last_indexf /. factor) |> Float.abs in 
-  Audio.add_coeff buf ((inc_coeff -. dec_coeff) |> Float.abs) buf
+(* Mutates an array in-place such that each element is the average of
+   itself and the next n elements. *)
+(* let blur n a = for i = 0 to Array.length a - 1 - n do a.(i) <- (a.(i)
+   +. a.(i + n)) /. float_of_int n done *)
 
-(*let envelope (factor : int) (index : int) last_index buf = 
-  let inc_coeff = index mod (last_index) / factor |> abs in 
-  let dec_coeff = (last_index - index) mod (last_index / factor) |> abs in 
-  Audio.add_coeff buf ((inc_coeff - dec_coeff) |> abs |> float_of_int) buf*)
-=======
+(* Smoothing lowpass filter example adapted to OCaml from:
+   http://phrogz.net/js/framerate-independent-low-pass-filter.html*)
+(* let smooth (smoothing : float) (values : float array) = for i = 0 to
+   Array.length values - 1 do let current_value = values.(i) in let
+   new_value = current_value *. (smoothing ** float_of_int i) in
+   values.(i) <- new_value done *)
+
+let envelope (factor : float) (index : int) last_index buf =
+  let indexf = float_of_int index in
+  let last_indexf = float_of_int last_index in
+  let inc_coeff = mod_float indexf last_indexf /. factor |> Float.abs in
+  let dec_coeff =
+    mod_float (last_indexf -. indexf) (last_indexf /. factor)
+    |> Float.abs
+  in
+  Audio.add_coeff buf (inc_coeff -. dec_coeff |> Float.abs) buf
+
+(*let envelope (factor : int) (index : int) last_index buf = let
+  inc_coeff = index mod (last_index) / factor |> abs in let dec_coeff =
+  (last_index - index) mod (last_index / factor) |> abs in
+  Audio.add_coeff buf ((inc_coeff - dec_coeff) |> abs |> float_of_int)
+  buf*)
 let all_channels array func =
   for c = 0 to Array.length array - 1 do
     func array.(c)
@@ -109,7 +111,6 @@ let envelope (index : int) (last_index : int) (factor : float) buf =
   in
   Audio.add_coeff buf (inc_coeff -. dec_coeff |> Float.abs) buf;
   buf
->>>>>>> Stashed changes
 
 (* let envelope (factor : int) (index : int) last_index buf = let
    inc_coeff = index mod last_index / factor |> abs in let dec_coeff =
@@ -124,25 +125,64 @@ let get_generator = function
   | "sine" -> new Audio.Mono.Generator.sine
   | _ -> new Audio.Mono.Generator.sine
 
-<<<<<<< Updated upstream
-let add_waves w1 w2 sum = 
+(* let play_using_generator input = let total_duration = snd input in
+   let channels = 2 in let sample_rate = 44100 in let frequency = fst
+   input in let wav = new Audio.IO.Writer.to_wav_file channels
+   sample_rate "out.wav" in let blen = 1024 in let wave = get_wave (thd
+   input) in let sound = Synth__Sound.new_wave wave frequency
+   sample_rate channels blen in let buf = Sound.get_buf sound in let
+   filter = (frth input) in let generator = new Audio.Generator.of_mono
+   (wave sample_rate freque) in let ao = new Mm_ao.writer channels
+   sample_rate in let wav = new Audio.IO.Writer.to_wav_file channels
+   sample_rate "out.wav" in let blen = 1024 in let buf = Audio.create
+   channels blen in let wave = get_generator (thd input) in let filter =
+   (frth input) in let generator = new Audio.Generator.of_mono (wave
+   sample_rate frequency) in let last_index = (sample_rate / blen *
+   total_duration) - 1 in for (i = 0 to last_index do Envelope envelope
+   10. i last_index buf;
+
+   let a = Audio.to_array buf in blur 2 |> all_channels a; smooth 5. |>
+   all_channels a; let buf = Audio.of_array a in
+
+   (* Create abstract Audio.t from array *) wav#write buf (* Sound.start
+   sound *) done; wav#close *)
+(* Sound.release sound*)
+
+(* let play_using_synth input = let total_duration = snd input in let
+   frequency = fst input in let channels = 4 in let sample_rate = 44100
+   in let blen = sample_rate * total_duration in let wave = get_wave
+   (thd input) in let sound = Synth__Sound.new_wave wave frequency
+   sample_rate channels blen in Synth__Sound.start sound *)
+
+(* Use this to switch between playing a whole buffer or looping over a
+   generator
+
+   So apparently Synth just uses generator *)
+let add_waves w1 w2 sum =
   let max = ref 0. in
-  for i = 0 to Array.length w1 - 1 do 
-    let value = w1.(i) +. w2.(i) in 
-    if value > !max then max := value; 
+  for i = 0 to Array.length w1 - 1 do
+    let value = w1.(i) +. w2.(i) in
+    if value > !max then max := value;
     sum.(i) <- value
   done;
-  for i = 0 to Array.length w1 - 1 do 
+  for i = 0 to Array.length w1 - 1 do
     sum.(i) <- sum.(i) /. !max
   done
 
-let play_using_generator input = 
+let sample_rate = 44100
+let fst (x, _, _, _) = x
+let snd (_, x, _, _) = x
+let thd (_, _, x, _) = x
+let frth (_, _, _, x) = x
+
+let play_using_generator input =
+  let freq = fst input in
   let total_duration = snd input in
   let channels = 2 in
-  let sample_rate = 44100 in
-  let freq = fst input in
   let ao = new Mm_ao.writer channels sample_rate in
-  let wav = new Audio.IO.Writer.to_wav_file channels sample_rate "out.wav" in
+  let wav =
+    new Audio.IO.Writer.to_wav_file channels sample_rate "out.wav"
+  in
   let blen = 1024 in
   let buf1 = Audio.create channels blen in
   let buf2 = Audio.create channels blen in
@@ -159,82 +199,29 @@ let play_using_generator input =
   for i = 0 to last_index do
     generator1#fill buf1;
     generator2#fill buf2;
-    (* Envelope *)
-    (*envelope 10. i last_index buf;*)
 
     (* Avg filter *)
-    
-    let a1 = Audio.to_array buf1 in (* Get array of raw data *)
+    let a1 = Audio.to_array buf1 in
+    (* Get array of raw data *)
     let a2 = Audio.to_array buf2 in
     let sum_c1 = Array.make (Array.length a1.(0)) 0. in
     let sum_c2 = Array.make (Array.length a1.(1)) 0. in
     add_waves a1.(0) a2.(0) sum_c1;
     add_waves a1.(1) a2.(1) sum_c2;
     let sum_array = [| sum_c1; sum_c2 |] in
-    
-    (*blur 2 |> all_channels a;*) (* Smoothing using my blur *)
-    smooth (-0.9) |> all_channels a1; (* Smoothing using smooth example *)
-    (*let buf = Audio.of_array a1 in*) (* Create abstract Audio.t from array *) 
+
+    (*blur 2 |> all_channels a;*)
+    (* Smoothing using my blur *)
+    (*smooth 5. |> all_channels a;*)
+    (* Smoothing using smooth example *)
+    (*let buf = Audio.of_array a1 in*)
+    (* Create abstract Audio.t from array *)
     let buf1 = Audio.of_array sum_array in
 
     wav#write buf1;
-    ao#write buf1;
+    ao#write buf1
   done;
   wav#close;
   ao#close
 
-let play_using_synth input = 
-  let total_duration = snd input in
-  let frequency = fst input in
-  let channels = 4 in
-  let sample_rate = 44100 in
-  let blen = sample_rate * total_duration in
-  let wave = get_wave (thd input) in
-  let sound = Synth__Sound.new_wave wave frequency sample_rate channels blen in
-  Synth__Sound.start sound
-
-  (*  Use this to switch between playing a whole buffer or looping over a 
-      generator  
-      
-      So apparently Synth just uses generator *)
-let play_sound input =
-  (*play_using_synth input*)
-  play_using_generator input
-=======
-(* let play_using_generator input = let total_duration = snd input in
-   let channels = 2 in let sample_rate = 44100 in let frequency = fst
-   input in let wav = new Audio.IO.Writer.to_wav_file channels
-   sample_rate "out.wav" in let blen = 1024 in let wave = get_wave (thd
-   input) in (* let sound = Synth__Sound.new_wave wave frequency
-   sample_rate channels blen in *) (* let buf = Sound.get_buf sound in
-   *) (* let filter = (frth input) in *) (* let generator = new
-   Audio.Generator.of_mono (wave sample_rate freque) in *) let ao = new
-   Mm_ao.writer channels sample_rate in let wav = new
-   Audio.IO.Writer.to_wav_file channels sample_rate "out.wav" in let
-   blen = 1024 in let buf = Audio.create channels blen in let wave =
-   get_generator (thd input) in (* let filter = (frth input) in *) let
-   generator = new Audio.Generator.of_mono (wave sample_rate frequency)
-   in let last_index = (sample_rate / blen * total_duration) - 1 in for
-   i = 0 to last_index do (* Envelope *) (* envelope 10. i last_index
-   buf; *)
-
-   (* Avg filter *) let a = Audio.to_array buf in (* Get array of raw
-   data *) (*blur 2 |> all_channels a;*) (* Smoothing using my blur *)
-   (* smooth 5. |> all_channels a; *) (* Smoothing using smooth example
-   *) let buf = Audio.of_array a in
-
-   (* Create abstract Audio.t from array *) wav#write buf (* Sound.start
-   sound *) done; wav#close *)
-(* Sound.release sound *)
-
-(* let play_using_synth input = let total_duration = snd input in let
-   frequency = fst input in let channels = 4 in let sample_rate = 44100
-   in let blen = sample_rate * total_duration in let wave = get_wave
-   (thd input) in let sound = Synth__Sound.new_wave wave frequency
-   sample_rate channels blen in Synth__Sound.start sound *)
-
-(* Use this to switch between playing a whole buffer or looping over a
-   generator
-
-   So apparently Synth just uses generator *)
->>>>>>> Stashed changes
+let play_sound input = play_using_generator input
